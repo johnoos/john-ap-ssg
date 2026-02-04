dcf650fe3bf4ea254f879118ec60a2a33690811d
joosthuizen <65812622+johnoos@users.noreply.github.com>
2026-02-04 12:04:15 +0200

    starting to work under the new architecture

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,13 +1,11 @@
-export default function(eleventyConfig) {
-  // 1. All configuration methods MUST be inside this function
-  eleventyConfig.addPassthroughCopy("src/assets/img");
-  eleventyConfig.addPassthroughCopy("src/assets/pdfs");
+export default async function(eleventyConfig) {
+  // Copy your CSS
+  eleventyConfig.addPassthroughCopy("src/css");
 
-  // 2. Return the configuration object at the end
   return {
     dir: {
       input: "src",
-      includes: "_parts" // Located at src/_parts/
+      output: "_site"
     }
   };
 };
