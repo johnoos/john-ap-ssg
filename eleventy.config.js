752a431fd6e0f6f53f725128825f6891a365152c
joosthuizen <65812622+johnoos@users.noreply.github.com>
2026-02-07 16:58:49 +0200

    final commit before switching over to fragment-gallery

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,8 @@
 export default async function(eleventyConfig) {
   // Copy your CSS
   eleventyConfig.addPassthroughCopy("src/css");
+  // This tells 11ty: "Take everything INSIDE src/assets and put it in _site/assets"
+eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
 
   return {
     dir: {
